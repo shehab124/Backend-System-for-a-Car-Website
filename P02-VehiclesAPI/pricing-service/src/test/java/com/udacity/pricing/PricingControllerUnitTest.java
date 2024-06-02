98308bf4363968ee0b535b43e0ccb1cd9de0f857
chehab <chehab.gamal@gmail.com>
2024-06-02 21:45:53 +0300

    done

---
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingControllerUnitTest.java
@@ -0,0 +1,38 @@
+package com.udacity.pricing;
+
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PricingService pricingService;
+
+    @Test
+    public void getPrice() throws Exception
+    {
+        mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk());
+
+        verify(pricingService, times(1)).getPrice(1L);
+
+    }
+
+}
